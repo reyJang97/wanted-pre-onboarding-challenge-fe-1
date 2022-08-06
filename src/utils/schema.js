01fe3d7614a2d3885756025b2b4df79218073b7d
reyJang97 <sooyeon.rok@gmail.com>
2022-08-06 16:38:44 +0900

    Updated: added validation for sign up

--- src/utils/schema.js
+++ src/utils/schema.js
@@ -10,3 +10,14 @@ export const signInYup = yup.object().shape({
     .required("This field is required")
     .min(8, "minimum 8 characters long"),
 });
+
+export const signUpYup = yup.object().shape({
+  email: yup
+    .string()
+    .required("This field is required")
+    .email("please check your email"),
+  password: yup
+    .string()
+    .required("This field is required")
+    .min(8, "minimum 8 characters long"),
+});
