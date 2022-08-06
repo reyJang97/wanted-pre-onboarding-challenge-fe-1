b85eda35896bd11b631e3efcffd408c17456bf2e
reyJang97 <sooyeon.rok@gmail.com>
2022-08-06 16:34:30 +0900

    Modified: added routes in App.js

--- src/App.js
+++ src/App.js
@@ -1,45 +1,19 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
-import { useForm } from "react-hook-form";
-import { yupResolver } from "@hookform/resolvers/yup";
-import { signInYup } from "./utils/schema";
-import { Button, Card, Space } from "antd";
-import InputField from "./components/FormFields/InputField";
-import PasswordField from "./components/FormFields/PasswordField";
+import NavBar from "./components/NavBar";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
 
 function App() {
-  const {
-    handleSubmit,
-    control,
-    formState: { errors },
-  } = useForm({
-    resolver: yupResolver(signInYup),
-    mode: "onBlur",
-  });
-
-  const onSubmit = (data) => {
-    alert("Success");
-  };
-
   return (
-    <div className="App">
-      <Card title="Welcome to your ToDo Manager!" size="large">
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <Space direction="vertical" size={20}>
-            <section>
-              <Space direction="vertical" size={15}>
-                <InputField control={control} errors={errors} />
-                <PasswordField control={control} errors={errors} />
-              </Space>
-            </section>
-            <section>
-              <Button>Sign In</Button>
-              <Button type="link">Wanna make a new account?</Button>
-            </section>
-          </Space>
-        </form>
-      </Card>
-    </div>
+    <>
+      <NavBar />
+      <Routes>
+        <Route path="/auth" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+    </>
   );
 }
 
