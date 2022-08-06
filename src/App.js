019348c954abf97fd5dd3a4371888e44a3fbcd77
reyJang97 <sooyeon.rok@gmail.com>
2022-08-06 19:37:22 +0900

    ADD: added ToDo List component and updated route in App.js

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import "./App.css";
 import NavBar from "./components/NavBar";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
+import ToDoList from "./pages/ToDoList";
 
 function App() {
   return (
     <>
       <NavBar />
       <Routes>
+        <Route path="/" element={<ToDoList />} />
         <Route path="/auth" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
       </Routes>
---
+++ src/pages/ToDoList.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ToDoList() {
+  return <div>Todos!</div>;
+}
+
+export default ToDoList;
