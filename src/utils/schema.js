6e3d5137f2fbd233d70acb5700fb32c5a5fa036f
reyJang97 <sooyeon.rok@gmail.com>
2022-08-06 13:55:54 +0900

    ADD: validation schema for yup

---
+++ src/utils/schema.js
@@ -0,0 +1,12 @@
+import * as yup from "yup";
+
+export const signInYup = yup.object().shape({
+  email: yup
+    .string()
+    .required("This field is required")
+    .email("please check your email"),
+  password: yup
+    .string()
+    .required("This field is required")
+    .min(8, "minimum 8 characters long"),
+});
