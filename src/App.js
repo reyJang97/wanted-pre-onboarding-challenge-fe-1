2f0df03f4c0d2b9d7e9f3da9080c2c85b71d984b
reyJang97 <sooyeon.rok@gmail.com>
2022-08-06 13:56:31 +0900

    ADD: main page (sign in)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,46 @@
 import React from "react";
+import "./App.css";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { signInYup } from "./utils/schema";
+import { Button, Card, Space } from "antd";
+import InputField from "./components/FormFields/InputField";
+import PasswordField from "./components/FormFields/PasswordField";
 
 function App() {
-  return <div className="App">Hello World!</div>;
+  const {
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(signInYup),
+    mode: "onBlur",
+  });
+
+  const onSubmit = (data) => {
+    alert("Success");
+  };
+
+  return (
+    <div className="App">
+      <Card title="Welcome to your ToDo Manager!" size="large">
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <Space direction="vertical" size={20}>
+            <section>
+              <Space direction="vertical" size={15}>
+                <InputField control={control} errors={errors} />
+                <PasswordField control={control} errors={errors} />
+              </Space>
+            </section>
+            <section>
+              <Button>Sign In</Button>
+              <Button type="link">Wanna make a new account?</Button>
+            </section>
+          </Space>
+        </form>
+      </Card>
+    </div>
+  );
 }
 
 export default App;
