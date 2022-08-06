29d4bae5b14a1b483f9acccdb020b6e9b20c9f02
reyJang97 <sooyeon.rok@gmail.com>
2022-08-06 16:35:54 +0900

    ADD: Navbar component

---
+++ src/components/NavBar.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { PageHeader } from "antd";
+import "../styles/NavBar.css";
+
+function NavBar() {
+  return <PageHeader className="nav-bar" title="ToDo Manager" />;
+}
+export default NavBar;
