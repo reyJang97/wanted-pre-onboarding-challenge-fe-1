d9d1e73a7ecba0dd6f04ff67840e47990d646aee
reyJang97 <sooyeon.rok@gmail.com>
2022-08-06 19:38:02 +0900

    Modified: added post request in SignUp.js

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -4,6 +4,7 @@ import { useNavigate } from "react-router-dom";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { signUpYup } from "../utils/schema";
+import axios from "axios";
 import { Button, Card, Space } from "antd";
 import InputField from "../components/FormFields/InputField";
 import PasswordField from "../components/FormFields/PasswordField";
@@ -17,6 +18,27 @@ function SignUp() {
     resolver: yupResolver(signUpYup),
     mode: "onBlur",
   });
+
+  const navigate = useNavigate();
+
+  const onSubmit = async (data) => {
+    const userData = {
+      email: data.email,
+      password: data.password,
+    };
+    console.log(userData);
+
+    try {
+      const res = await axios.post(
+        "http://localhost:8080/users/create",
+        userData
+      );
+      alert(res.data.message);
+      navigate("/");
+    } catch (error) {
+      alert(error);
+    }
+  };
 
   return (
     <div className="sign-up">
@@ -34,7 +56,7 @@ function SignUp() {
               </Space>
             </section>
             <section>
-              <Button type="primary" block>
+              <Button type="primary" onClick={handleSubmit(onSubmit)} block>
                 Let's get started!
               </Button>
             </section>
