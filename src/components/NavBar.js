6923100576303cf6ecbf7fcf36d24f977f4ee23d
reyJang97 <sooyeon.rok@gmail.com>
2022-08-07 18:57:50 +0900

    Modified Navbar: added go to main icon

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,23 @@
 import React from "react";
 import { PageHeader } from "antd";
 import "../styles/NavBar.css";
+import { useNavigate } from "react-router-dom";
+import { CalendarOutlined } from "@ant-design/icons";
 
 function NavBar() {
-  return <PageHeader className="nav-bar" title="ToDo Manager" />;
+  const navigate = useNavigate();
+
+  const goToMain = () => {
+    navigate("/");
+  };
+
+  return (
+    <PageHeader
+      className="nav-bar"
+      title="ToDo Manager"
+      onBack={goToMain}
+      backIcon={<CalendarOutlined />}
+    />
+  );
 }
 export default NavBar;
