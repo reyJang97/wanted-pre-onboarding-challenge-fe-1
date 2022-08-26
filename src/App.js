f1a5721be7a61be8028e1f4b9dd914cf17d68aea
sooyeanJang <sooyean.jang@gmail.com>
2022-08-26 14:42:53 +0900

    UPDATE: update routes

--- src/App.js
+++ src/App.js
@@ -2,18 +2,24 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import NavBar from "./components/NavBar";
+import Main from "./pages/Main";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import ToDoList from "./pages/ToDoList";
+import ToDo from "./pages/ToDo";
+import CreateToDo from "./pages/CreateToDo";
 
 function App() {
   return (
     <>
       <NavBar />
       <Routes>
-        <Route path="/" element={<ToDoList />} />
+        <Route path="/" element={<Main />} />
         <Route path="/auth" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
+        <Route path="/todos" element={<ToDoList />} />
+        <Route path="/todos/:id" element={<ToDo />} />
+        <Route path="/create" element={<CreateToDo />} />
       </Routes>
     </>
   );
